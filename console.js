7ac1d5ea959de9121f8ec58f02d2ea7ea81b5c39
Andy <andrew.sunglae.kim@gmail.com>
2016-06-06 15:30:50 -0400

    adds console

---
+++ console.js
@@ -0,0 +1,15 @@
+require("./helpers/helper")
+var repl = require("repl");
+var mongoose = require('mongoose')
+var conn = mongoose.connect('mongodb://localhost/poker')
+var Card = require("./models/card")
+var Player = require("./models/player")
+var Game = require("./models/game")
+
+var replServer = repl.start({
+  prompt: "Express Console > "
+})
+
+replServer.context.Card = Card;
+replServer.context.Player = Player;
+replServer.context.Game = Game;
